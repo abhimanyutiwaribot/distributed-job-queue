074c13b11ba9a009d48f69cc211acf0be3122b6b
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-06-09 18:19:40 +0530

    create job service added

--- backend/src/modules/job/job.service.ts
+++ backend/src/modules/job/job.service.ts
@@ -1,9 +1,16 @@
+import { prisma } from "../../database/db";
 import type { CreateJobInput } from "./job.validation";
 
 export async function createJobService(data: CreateJobInput){
   // this will do the database calls
-  
+  const job = await prisma.job.create({
+    data: {
+      job_type: data.jobType,
+      payload: data.payload,
+    }
+  })
 
+  return job;
 }
 
 export function getAllJobService(){
