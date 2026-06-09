5ffa245d4bf63a7049162176e084071d86e309ce
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-06-09 17:57:10 +0530

    add job service

---
+++ backend/src/modules/job/job.service.ts
@@ -0,0 +1,15 @@
+import type { CreateJobInput } from "./job.validation";
+
+export async function createJobService(data: CreateJobInput){
+  // this will do the database calls
+  
+
+}
+
+export function getAllJobService(){
+  // this will do the database calls
+}
+
+export function getJobByIdService(){
+  // this will do the database calls
+}
