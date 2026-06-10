ed4fc50994eaf121429a71725b032ef90a4b13ea
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-06-10 13:00:46 +0530

    added nodemailer sendEmail handler

--- backend/src/handler/sent-email.handler.ts
+++ backend/src/handler/sent-email.handler.ts
@@ -1,6 +1,30 @@
-export async function sendEmailHandler(payload: unknown){
-  console.log("Sending Email");
-  console.log(payload);
+import { transporter } from "../lib/mailer"
 
-  throw new Error("SMTP DOWN")
+export type SendEmailPayload = {
+  to: string,
+  subject: string,
+  body: string
 }
+
+export async function sendEmailHandler(payload: SendEmailPayload) {
+  try {
+    const send = await transporter.sendMail({
+      from: process.env.SMTP_USER,
+      to: payload.to,
+      subject: payload.subject,
+      text: payload.body
+    })
+
+    console.log(`Email sent: ${send.messageId}`)
+  } catch (error) {
+    console.error(`Failed to Send Email: ${error}`)
+  }
+}
+
+// await sendEmailHandler(
+//   {
+//   "to": "a95534402@gmail.com",
+//   "subject": "Test subject",
+//   "body": "Test message text"
+// }
+// )
