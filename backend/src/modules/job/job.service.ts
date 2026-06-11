fed7cd570cc09048ac53bd584ab4417641725e29
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-06-11 16:44:40 +0530

    idemKey to db

--- backend/src/modules/job/job.service.ts
+++ backend/src/modules/job/job.service.ts
@@ -1,12 +1,14 @@
+import type { ZodUUID } from "zod";
 import { prisma } from "../../database/db";
 import type { CreateJobInput } from "./job.validation";
 
-export async function createJobService(data: CreateJobInput){
+export async function createJobService(data: CreateJobInput, idemKey: string){
   // this will do the database calls
   const job = await prisma.job.create({
     data: {
       job_type: data.jobType,
       payload: data.payload,
+      idem_key: idemKey
     }
   })
 
