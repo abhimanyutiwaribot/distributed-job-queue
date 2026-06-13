de576ce43a5b3c42707b34080a286bda4f3a4b50
abhimanyutiwaribot <tiwariabhi2004@gmail.com>
2026-06-13 18:54:34 +0530

    cors added index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,21 @@
 import express from "express";
+import cors from 'cors';
 import { jobRouter } from "./modules/job/job.route.ts";
 import { dlqRouter } from "./modules/dlq/dlq.route.ts";
 import { metricsRouter } from "./modules/metrics/metrics.route.ts";
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+//for production
+// const corsOptions = {
+//   origin: ['https://frontend.com'], // Allowed domains
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true, // Allow cookies/auth headers
+//   optionsSuccessStatus: 200 // For legacy browser support
+// };
 
+app.use(cors())
+const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use("/api/v1", jobRouter);
 app.use("/api/v1/dlq", dlqRouter)
